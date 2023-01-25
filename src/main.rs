92e4c15897c7c74b270f1ce32c40e91c65c1c2af
amir <amir.w.designer@gmail.com>
2023-01-25 16:13:31 +0330

    Adding new&rotate functions, rotate function rotate image and flip one of them

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,10 @@
-use fltk::{app, frame::{Frame}, button::Button,  prelude::*, window::{Window, DoubleWindow}};
+use fltk::{
+    app,
+    button::Button,
+    frame::Frame,
+    prelude::*,
+    window::{DoubleWindow, Window},
+};
 use std::error::Error;
 mod img;
 
@@ -8,19 +14,15 @@ fn main() -> Result<(), Box<dyn Error>> {
     let mut wind = Window::default().with_size(350, 400);
     wind.set_color(fltk::enums::Color::from_rgb(255, 255, 255));
     let mut frame1 = Frame::default().with_size(150, 150).center_x(&wind);
-    let mut frame2 = Frame::default().with_size(150, 150).below_of(&frame1,0);
-    frame1.set_pos(0,20);
+    let mut frame2 = Frame::default().with_size(150, 150).below_of(&frame1, 0);
+    frame1.set_pos(0, 20);
 
-    let mut flip:u8 = 0; //We use this var to track image is flipped or not
-
-
+    let mut flip: u8 = 0; //We use this var to track image is flipped or not
 
     wind.end();
     wind.make_resizable(false);
     wind.show();
     app.run()?;
 
-
     Ok(())
 }
-
