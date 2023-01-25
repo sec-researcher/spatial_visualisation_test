44c4f130a84fb90cda2f99f28bd6c62180e196d9
amir <amir.w.designer@gmail.com>
2023-01-25 16:18:04 +0330

    updating main function to handle button click functionality

--- src/main.rs
+++ src/main.rs
@@ -8,22 +8,87 @@ use fltk::{
 use std::error::Error;
 mod img;
 
+#[derive(Debug, Clone, Copy)]
+pub enum Message {
+    Matched,
+    NotMatched,
+}
+
 fn main() -> Result<(), Box<dyn Error>> {
-    //Defining window and frames
     let app = app::App::default().with_scheme(app::Scheme::Gleam);
     let mut wind = Window::default().with_size(350, 400);
     wind.set_color(fltk::enums::Color::from_rgb(255, 255, 255));
     let mut frame1 = Frame::default().with_size(150, 150).center_x(&wind);
     let mut frame2 = Frame::default().with_size(150, 150).below_of(&frame1, 0);
     frame1.set_pos(0, 20);
+    let mut flip: u8 = 0;
+    update(&mut wind, &mut frame1, &mut frame2, &mut flip);
 
-    let mut flip: u8 = 0; //We use this var to track image is flipped or not
+    let mut frame = Frame::default().with_size(100, 40);
+    frame.set_pos(200, 150);
 
+    frame.set_label_size(20);
+    let mut matched = Button::default()
+        .size_of(&frame)
+        .above_of(&frame, 0)
+        .with_label("Match");
+    let mut not_matched = Button::default()
+        .size_of(&frame)
+        .below_of(&frame, 0)
+        .with_label("Not match");
     wind.end();
     wind.make_resizable(false);
     wind.show();
-    app.run()?;
 
+    let (s, r) = app::channel::<Message>();
+    matched.emit(s, Message::Matched);
+    not_matched.emit(s, Message::NotMatched);
+
+    let mut mode = 0;
+    let default_color = matched.color();
+
+    while app.wait() {
+        if let Some(msg) = r.recv() {
+            match msg {
+                Message::Matched => {
+                    let mut btn = matched.clone();
+                    if mode == 0 {
+                        if flip==1 {
+                            btn.set_color(fltk::enums::Color::Green);
+                            btn.set_label("Right")
+                        } else {
+                            btn.set_color(fltk::enums::Color::Red);
+                            btn.set_label("Wrong")
+                        }
+                        mode = 1;
+                    } else {
+                        btn.set_color(default_color);
+                        btn.set_label("Match");
+                        mode = 0;
+                        update(&mut wind, &mut frame1, &mut frame2, &mut flip);
+                    }
+                }
+                Message::NotMatched => {
+                    let mut btn = not_matched.clone();
+                    if mode == 0 {
+                        if flip==1 {
+                            btn.set_color(fltk::enums::Color::Red);
+                            btn.set_label("Worng")
+                        } else {
+                            btn.set_color(fltk::enums::Color::Green);
+                            btn.set_label("Right")
+                        }
+                        mode = 1;
+                    } else {
+                        btn.set_color(default_color);
+                        btn.set_label("Not match");
+                        mode = 0;
+                        update(&mut wind, &mut frame1, &mut frame2, &mut flip);
+                    }
+                }
+            }
+        }
+    }
     Ok(())
 }
 
